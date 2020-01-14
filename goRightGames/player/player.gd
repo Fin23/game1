be79284a01f16533e6b5806eab8b1878acf66a0b
Ross Jameson <Rossjameson1623@gmail.com>
2020-01-14 01:50:16 -0600

    gave hunter animations

--- goRightGames/player/player.gd
+++ goRightGames/player/player.gd
@@ -4,11 +4,28 @@ extends KinematicBody2D
 
 const SPEED = 770
 var movedir = Vector2(0,0)
-
+var spritedir = "down"
 
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
+	spritedir_loop()
+	
+	if is_on_wall():
+		if spritedir == "left" and test_move(transform, Vector2(-1,0)):
+			anim_switch("push")
+		if spritedir == "right" and test_move(transform, Vector2(1,0)):
+			anim_switch("push")
+		if spritedir == "up" and test_move(transform, Vector2(0,-1)):
+			anim_switch("push")	
+		if spritedir == "down" and test_move(transform, Vector2(0, 1)):
+			anim_switch("push")	
+			
+	elif movedir != Vector2(0,0):
+		anim_switch("walk")
+	else:
+		anim_switch("idle")
+
 
 func controls_loop():
 	var LEFT	= Input.is_action_pressed("ui_left")
@@ -23,4 +40,20 @@ func movement_loop():
 	var motion = movedir.normalized() * SPEED
 	move_and_slide(motion, Vector2(0,0))
 	
-#================== end of movement fns ========
+func spritedir_loop():
+	match movedir:
+		Vector2(-1,0):
+			spritedir = "left"
+		Vector2(1,0):
+			spritedir = "right"
+		Vector2(0,-1):
+			spritedir = "up"
+		Vector2(0,1):
+			spritedir = "down"
+	
+func anim_switch(animation):
+	var newanim = str(animation, spritedir)
+	if $anim.current_animation != newanim:
+		$anim.play(newanim)
+	
+	#================== end of movement fns ========
